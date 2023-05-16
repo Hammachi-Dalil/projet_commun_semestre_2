05c89852f9229169cc16e5516f1b2bb7e4d1ec45
Hammachi-Dalil <116789242+Hammachi-Dalil@users.noreply.github.com>
2023-05-16 10:19:22 +0200

    npm run dev fonctionne

--- projet_commun_semestre2/src/App.vue
+++ projet_commun_semestre2/src/App.vue
@@ -5,10 +5,12 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="./assets/css/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
+
+      <div class="wrapper bg-red-800 p-6"></div>
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
