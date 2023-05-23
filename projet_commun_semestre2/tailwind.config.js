3aeaa0a576049e79f215e0a995ecb3dc98f04082
Hammachi-Dalil <116789242+Hammachi-Dalil@users.noreply.github.com>
2023-05-23 17:09:07 +0200

    ajout des couleur ( à définir)

--- projet_commun_semestre2/tailwind.config.js
+++ projet_commun_semestre2/tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     "./index.html",
     "./src**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      // rajouter les couleurs à utiliser
+    },
     extend: {},
   },
   plugins: [],
