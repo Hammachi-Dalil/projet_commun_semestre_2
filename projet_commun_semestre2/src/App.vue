80a215eed367001551538659c8da8226ad8a6bc9
Hammachi-Dalil <116789242+Hammachi-Dalil@users.noreply.github.com>
2023-05-23 16:55:48 +0200

    update de app.vue pour intégrer header et footer

--- projet_commun_semestre2/src/App.vue
+++ projet_commun_semestre2/src/App.vue
@@ -1,15 +1,13 @@
 <script setup lang="ts">
-	import { RouterLink, RouterView } from 'vue-router'
+	import { RouterView } from 'vue-router'
+  import Header from './components/HeaderPage.vue'
+  import Footer from './components/FooterPage.vue'
 </script>
 
 <template>
-  <header>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-			<RouterLink to="/events">Agenda</RouterLink>
-      <RouterLink to="/contact">Contact</RouterLink>
-    </nav>
-  </header>
-
-  <RouterView />
+  <Header />
+    <main>
+      <RouterView />
+    </main>
+  <Footer />
 </template>
